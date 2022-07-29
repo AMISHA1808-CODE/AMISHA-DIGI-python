cf0a0986b61ac3fef9ef1a143027518384cf4e97
AMISHA1808-CODE <107685999+AMISHA1808-CODE@users.noreply.github.com>
2022-07-29 14:11:20 +0530

    function

--- strinffun.py
+++ strinffun.py
@@ -16,9 +16,17 @@ def word_count(text):
         else:
             wc[word]=1
     return wc  
+def sort(word_dict):
+    ans =sorted(word_dict.items(),key=lambda val:val[1],reverse=True)    
+    return dict(ans)#convert list to dic
 
 if __name__=='__main__':
-   long_text=''''''
-    cln_text=remove_punc(msg)
-    print('original:',msg)
-    print('cleaned:',cln_text)
+   long_text='''
+   The student of twelth class are going to picnic they all have to bring thier 
+   lunch and towels with them and the have to all follow all rules. 
+   if they do not follow rules then they have to give fine
+   '''
+   cln_text=remove_punc(long_text)
+   counted_word=word_count(cln_text)
+   sorted_words=sort(counted_word)
+   print(sorted_words)
