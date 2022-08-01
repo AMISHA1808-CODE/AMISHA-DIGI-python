cb8c335868a503b97cbcedbee508527a4c4345ca
AMISHA1808-CODE <107685999+AMISHA1808-CODE@users.noreply.github.com>
2022-08-01 15:47:53 +0530

    game.py

---
+++ g5.py
@@ -0,0 +1,19 @@
+import turtle
+car=turtle.Turtle()
+car.color('yellow')
+car.fillcolor('red')
+car.penup()
+car.goto(0,0)
+car.pendown()
+car.begin_fill()
+car.forward(370)
+car.left(90)
+car.forward(50)
+car.left(90)
+car.forward(370)
+car.left(90)
+car.forward(50)
+car.end_fill()
+
+
+
